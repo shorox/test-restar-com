c872b5c505d03c6359789b8ef1b259b492876a91
shorox <skazo4nix@gmail.com>
2018-07-04 23:54:21 +0300

    https://spring.io/guides/gs/consuming-rest/

--- src/main/java/com/retar/test/testrestarcom/constroller/GreetingController.java
+++ src/main/java/com/retar/test/testrestarcom/constroller/GreetingController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class GreetingController {
 
     private static final String TEMPLATE = "Hello, %s!";
     private final AtomicLong COUNTER = new AtomicLong();
 
-    @RequestMapping("/Greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(COUNTER.incrementAndGet(), String.format(TEMPLATE, name));
-    }
+//    @RequestMapping("/Greeting")
+//    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+//        return new Greeting(COUNTER.incrementAndGet(), String.format(TEMPLATE, name));
+//    }
 }
